159f8cf93a72ac6b1b50a40ac997bbd444ec254e
David Moraes <investtraderrs@gmail.com>
2020-09-16 14:32:35 -0300

    Acrescentada mensagem de BOM DIA

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -7,6 +7,7 @@ namespace TesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia!");
 
             Console.WriteLine();
         }
